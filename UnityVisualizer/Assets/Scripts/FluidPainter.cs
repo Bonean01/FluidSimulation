9de1d738b6a02e62ee6fa33827440b85c1a65adb
Borja Benedito <borbeju@gmail.com>
2026-07-17 08:21:34 +0200

    The brush size (radius) in the fluid painter is now interpreted in world coordinates

--- UnityVisualizer/Assets/Scripts/FluidPainter.cs
+++ UnityVisualizer/Assets/Scripts/FluidPainter.cs
@@ -14,6 +14,7 @@ public class FluidPainter : MonoBehaviour {
     [SerializeField] private float brushSize;
     [SerializeField] [Range(0, 1)] private float brushStrength;
     private FluidSimulationAdapter m_simulationAdapter;
+    private float m_brushSizeGrid;
 
 
     private void Awake() {
@@ -21,16 +22,14 @@ public class FluidPainter : MonoBehaviour {
     }
 
 
-    private Vector2 m_prevMousePosWorld, m_mouseWorldDelta;
+    private Vector2 m_prevMousePosGrid, m_mouseGridDelta;
     private void Update() {
         if (!IsWithinBounds(Input.mousePosition)) return;
 
-        Vector2 mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePosGrid = m_simulationAdapter.WorldToGridPoint(mousePosWorld);
-        
-        m_mouseWorldDelta = mousePosWorld - m_prevMousePosWorld;
+        Vector2 mousePosGrid = WorldToSimulationGrid(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        m_mouseGridDelta = mousePosGrid - m_prevMousePosGrid;
         if (Input.GetMouseButton(0)) Paint(mousePosGrid);
-        m_prevMousePosWorld = mousePosWorld;
+        m_prevMousePosGrid = mousePosGrid;
     }
 
 
@@ -43,11 +42,27 @@ public class FluidPainter : MonoBehaviour {
     }
 
 
+    private Vector2Int WorldToSimulationGrid(Vector2 worldPos) {
+        float widthWorld = transform.localScale.x;
+        int widthGrid = m_simulationAdapter.Width;
+        int heightGrid = m_simulationAdapter.Height;
+        float cellWidthWorld = widthWorld / widthGrid;
+        return new() {
+            x = (int)(worldPos.x / cellWidthWorld + widthGrid / 2),
+            y = (int)(worldPos.y / cellWidthWorld + heightGrid / 2)
+        };
+    }
+
+
     private void Paint(Vector2 mousePosGrid) {
-        Vector2 mouseVel = m_mouseWorldDelta / Time.deltaTime;
+        Vector2 mouseVel = m_mouseGridDelta / Time.deltaTime; // we also need to take the cellsize into consideration
         mouseVel *= brushStrength;
         Vec2f velocity = new(mouseVel.x, mouseVel.y);
 
-        m_simulationAdapter.ApplyVelocityImpulse(mousePosGrid, velocity, brushSize);
+        float widthWorld = transform.localScale.x;
+        int widthGrid = m_simulationAdapter.Width;
+        m_brushSizeGrid =  brushSize * widthGrid / widthWorld;
+
+        m_simulationAdapter.ApplyVelocityImpulse(mousePosGrid, velocity, m_brushSizeGrid);
     }
 }
