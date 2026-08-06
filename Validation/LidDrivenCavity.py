9cec5ddb45e1c3570539e37469469de696f5c926
Borja Benedito <borbeju@gmail.com>
2026-08-06 10:42:20 +0200

    Added wrapper types for the simulation in python

--- Validation/LidDrivenCavity.py
+++ Validation/LidDrivenCavity.py
@@ -1,73 +1,91 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-from ctypes import *
-from enum import IntEnum
+from SimulationWrapperTypes import *
 
-
-class Vec2f(Structure):
-    _fields_ = [
-        ("x", c_float),
-        ("y", c_float)
-    ]
-
-class CellType(IntEnum):
-    FLUID = 0
-    SOLID = 1
-
-class BoundaryType(IntEnum):
-    NONE = 0
-    NO_SLIP = 1
-    FREE_SLIP = 2
-    INLET = 3
-    OUTLET = 4
-
-class CellData(Structure):
-    _fields_ = [
-        ("cell_type", c_uint8),
-        ("boundary_type", c_uint8),
-        ("prescribed_velocity", Vec2f)
-    ]
-
-
-libc: CDLL = cdll.LoadLibrary("../FluidSolverCore/out/build/debug/FluidSolver.dll")
 
 side_length: int = 33
-width: c_int = side_length
-height: c_int = side_length
 cell_width: c_float = 1.0 / side_length
 density: c_float = 1.0
 kinematic_viscosity: c_float = 1e-05
 iteration_count: c_int = 30
 
+lid_speed: int = 1.0
 time_step: c_float = 1.0 / 120.0
 simulation_iteration_count: int = 1000
 
-wallData: CellData = CellData(CellType.SOLID, BoundaryType.NO_SLIP, Vec2f(0.0, 0.0))
-lidData: CellData = CellData(CellType.SOLID, BoundaryType.NO_SLIP, Vec2f(1.0, 0.0))
+
+for i, string in enumerate(sys.argv):
+    if i == 0 or not string.startswith("--"):
+        continue
+
+    match string:
+        case "--grid-side-length":
+            side_length = c_int(sys.argv[i + 1])
+        case "--cell-width":
+            cell_width = c_float(sys.argv[i + 1])
+        case "--density":
+            density = c_float(sys.argv[i + 1])
+        case "--kinematic-viscosity":
+            kinematic_viscosity = c_float(sys.argv[i + 1])
+        case "--lid-speed":
+            lid_speed = int(sys.argv[i + 1])
+        case "--time-step":
+            time_step = c_float(sys.argv[i + 1])
+        case "--iterations":
+            simulation_iteration_count = int(sys.argv[i + 1])
+        case _:
+            print(
+                f"Unsupported option: {string}\n" +
+                "Available options:\n" +
+                "================================================\n" +
+                "--grid-side-length\n" +
+                "--cell-width\n" +
+                "--density\n" +
+                "--kinematic-viscosity\n" +
+                "--lid-speed\n" +
+                "--time-step\n" +
+                "--iterations\n" + 
+                "================================================\n"
+            )
+            quit()
+
+
+print(
+    "\nRunning lid-driven cavity with parameters:\n" +
+    "================================================\n" +
+    f"grid_side_length: {side_length}\n" +
+    f"cell_width: {cell_width}\n"
+    f"density: {density}\n" +
+    f"kinematic_viscosity: {kinematic_viscosity}\n" +
+    f"lid_speed: {lid_speed}\n" +
+    f"time_step: {time_step}\n" +
+    f"iterations: {simulation_iteration_count}\n" +
+    "================================================\n"
+)
 
 
 print("Setting up...")
-libc.CreateSimulation.argtypes = [c_int, c_int, c_float, c_float, c_float, c_uint32]
-libc.CreateSimulation.restype = c_void_p
+width: c_int = side_length
+height: c_int = side_length
+simulation: FluidSimulation = FluidSimulation(width, height, cell_width, density, kinematic_viscosity, iteration_count)
 
-handle: c_void_p = libc.CreateSimulation(width, height, cell_width, density, kinematic_viscosity, iteration_count)
-libc.SetCellData.argtypes = [c_void_p, c_int, c_int, CellData]
-libc.SetCellData.restype = c_void_p
+lid_data: CellData = CellData(CellType.SOLID, BoundaryType.NO_SLIP, Vec2f(lid_speed, 0.0))
+wall_data: CellData = CellData(CellType.SOLID, BoundaryType.NO_SLIP, Vec2f(0.0, 0.0))
+
 for i in range(width):
-    libc.SetCellData(handle, i, height - 1, lidData)
-    libc.SetCellData(handle, i, 0, wallData)
+    simulation.SetCellData(i, height - 1, lid_data)
+    simulation.SetCellData(i, 0, wall_data)
 
 for j in range(height - 1):
-    libc.SetCellData(handle, 0, j, wallData)
-    libc.SetCellData(handle, width - 1, j, wallData)
+    simulation.SetCellData(0, j, wall_data)
+    simulation.SetCellData(width - 1, j, wall_data)
 
 
 print("Running simulation...")
-libc.Step.argtypes = [c_void_p, c_float]
-libc.Step.restype = c_void_p
 for k in range(simulation_iteration_count):
-    libc.Step(handle, time_step)
+    simulation.Step(time_step)
 
 
 print("Drawing results...")
@@ -77,11 +95,9 @@ U, V = np.meshgrid(X, Y)
 
 fig, ax = plt.subplots()
 
-libc.GetVelocity.argtypes = [c_void_p, c_int, c_int]
-libc.GetVelocity.restype = Vec2f
 for i in range(width):
     for j in range(height):
-        vel: Vec2f = libc.GetVelocity(handle, i, j)
+        vel: Vec2f = simulation.GetVelocity(i, j)
         ax.quiver(1.0 / width * i, 1.0 / height * j, vel.x, vel.y)
 
 plt.show()
---
+++ Validation/SimulationWrapperTypes.py
@@ -0,0 +1,77 @@
+import os
+
+from ctypes import *
+from enum import IntEnum
+
+
+class Vec2f(Structure):
+    _fields_ = [
+        ("x", c_float),
+        ("y", c_float)
+    ]
+
+class CellType(IntEnum):
+    FLUID = 0
+    SOLID = 1
+
+class BoundaryType(IntEnum):
+    NONE = 0
+    NO_SLIP = 1
+    FREE_SLIP = 2
+    INLET = 3
+    OUTLET = 4
+
+class CellData(Structure):
+    _fields_ = [
+        ("cell_type", c_uint8),
+        ("boundary_type", c_uint8),
+        ("prescribed_velocity", Vec2f)
+    ]
+
+
+class FluidSimulation:
+    libc: CDLL
+    handle: c_void_p
+
+    def __init__(self, width: c_int, height: c_int, cell_width: c_float, density: c_float, kinematic_viscosity: c_float, iteration_count: c_uint32):
+        path = "../FluidSolverCore/out/build/debug/"
+        if os.path.exists(path + "FluidSolver.dll"):
+            self.libc = cdll.LoadLibrary(path + "FluidSolver.dll")
+        elif os.path.exists(path + "libFluidSolver.so"):
+            self.libc = cdll.LoadLibrary(path + "libFluidSolver.so")
+        else:
+            raise FileNotFoundError(f"Could not find FluidSolver.dll nor libFluidSolver.so at: {path}")
+
+        self.libc.CreateSimulation.argtypes = [c_int, c_int, c_float, c_float, c_float, c_uint32]
+        self.libc.CreateSimulation.restype = c_void_p
+        self.handle = self.libc.CreateSimulation(width, height, cell_width, density, kinematic_viscosity, iteration_count)
+
+
+    def __del__(self):
+        self.libc.DestroySimulation.argtypes = [c_void_p]
+        self.libc.DestroySimulation.restype = c_void_p
+        self.libc.DestroySimulation(self.handle)
+
+
+    def Step(self, time_step: c_float) -> None:
+        self.libc.Step.argtypes = [c_void_p, c_float]
+        self.libc.Step.restype = c_void_p
+        self.libc.Step(self.handle, time_step)
+
+
+    def GetVelocity(self, i: c_int, j: c_int) -> Vec2f:
+        self.libc.GetVelocity.argtypes = [c_void_p, c_int, c_int]
+        self.libc.GetVelocity.restype = Vec2f
+        return self.libc.GetVelocity(self.handle, i, j)
+
+
+    def SetVelocity(self, i: c_int, j: c_int, velocity: Vec2f) -> None:
+        self.libc.SetVelocity.argypes = [c_void_p, c_int, c_int, Vec2f]
+        self.libc.SetVelocity.restype = c_void_p
+        self.libc.SetVelocity(self.handle, i, j, velocity)
+
+
+    def SetCellData(self, i: c_int, j: c_int, cell_data: CellData) -> None:
+        self.libc.SetCellData.argtypes = [c_void_p, c_int, c_int, CellData]
+        self.libc.SetCellData.restype = c_void_p
+        self.libc.SetCellData(self.handle, i, j, cell_data)
